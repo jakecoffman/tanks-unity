59ef2f1ca7a85cf65c08f4ae668b32d239d31447
jake <jake@jakecoffman.com>
2015-11-28 16:09:55 -0600

    drivable tank

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 60f;
+    public float turnSpeed = 1.5f;
+
+    private float rotation = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // called once per physics tick
+    void FixedUpdate()
+    {
+        rotation += Input.GetAxis("Horizontal") * turnSpeed;
+        if (speed > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, -rotation);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, rotation);
+        }
+
+        //GetComponent<Rigidbody2D>().angularVelocity = 0;
+        float forwardInput = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * forwardInput);
+    }
+}
---
+++ Assets/scripts/PlayerTurret.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTurret : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var rot = Quaternion.LookRotation(transform.position - mousePos, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+    }
+}
