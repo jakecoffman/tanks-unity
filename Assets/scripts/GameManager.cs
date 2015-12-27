d5f8c41243c3f2c9f6596166c1b402dee9174cea
jake <jake@jakecoffman.com>
2015-12-27 15:59:47 -0600

    remove singleton to fix restarting match not creating board

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,33 +1,1 @@
-﻿using UnityEngine;
-using UnityEngine.Networking;
-using System.Collections;
-
-public class GameManager : NetworkBehaviour {
-    public static GameManager instance = null;
-    public BoardManager boardScript;
-
-    private int level = 3;
-
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else if(instance != this)
-        {
-            Destroy(gameObject);
-        }
-
-        DontDestroyOnLoad(gameObject);
-    }
-
-	public override void OnStartServer() {
-        if (!isServer)
-        {
-            return;
-        }
-        boardScript = GetComponent<BoardManager>();
-        boardScript.SetupScene(level);
-    }
-}
+using UnityEngine;using UnityEngine.Networking;using System.Collections;public class GameManager : NetworkBehaviour {    public BoardManager boardScript;    void Awake()    {        //if (instance == null)        //{        //    instance = this;        //}        //else if(instance != this)        //{        //    Destroy(gameObject);        //}        //DontDestroyOnLoad(gameObject);    }	public override void OnStartServer() {        if (!isServer)        {            return;        }        boardScript = GetComponent<BoardManager>();        boardScript.SetupScene();    }}
