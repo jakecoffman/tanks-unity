6292ab26b2edd881c76ff2eda50c0d3af9fe5dd3
jake <jake@jakecoffman.com>
2015-12-27 14:39:39 -0600

    had scripts in the wrong location

---
+++ Assets/scripts/MyNetworkDiscovery.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class MyNetworkDiscovery : NetworkDiscovery {
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        NetworkManager.singleton.networkAddress = fromAddress;
+        NetworkManager.singleton.StartClient();
+    }
+}
