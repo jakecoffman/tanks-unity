8d2e98246825ed9e76dc9bb4a2e3aaef261fdc28
jake <jake@jakecoffman.com>
2015-12-31 21:09:27 -0600

    Fixed aiming from a distance and fixed bullet timeout running out of ammo.

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -1,48 +1,1 @@
-﻿using UnityEngine;
-using UnityEngine.Networking;
-using System.Collections;
-
-public class Bullet : NetworkBehaviour {
-
-    [HideInInspector]
-    public PlayerMovement player;
-
-    void Start()
-    {
-        StartCoroutine(Destroy());
-    }
-
-    IEnumerator Destroy()
-    {
-        yield return new WaitForSeconds(2.0f);
-        Destroy(gameObject);
-    }
-
-    public override void OnNetworkDestroy()
-    {
-        Destroy(gameObject);
-    }
-
-    [ServerCallback]
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (!isServer)
-        {
-            return;
-        }
-        var hit = collider.gameObject;
-        if (hit.tag == "Player")
-        {
-            var combat = hit.GetComponent<Combat>();
-            combat.TakeDamage(10);
-            player.numBullets--;
-            NetworkServer.Destroy(gameObject);
-        }
-        else if (hit.tag == "Wall")
-        {
-            Debug.Log("Wall hit");
-            player.numBullets--;
-            NetworkServer.Destroy(gameObject);
-        }
-    }
-}
+using UnityEngine;using UnityEngine.Networking;using System.Collections;public class Bullet : NetworkBehaviour {    [HideInInspector]    public PlayerMovement player;    void Start()    {        StartCoroutine(Destroy());    }    IEnumerator Destroy()    {        yield return new WaitForSeconds(2.0f);        player.numBullets--;        Destroy(gameObject);    }    public override void OnNetworkDestroy()    {        Destroy(gameObject);    }    [ServerCallback]    void OnTriggerEnter2D(Collider2D collider)    {        if (!isServer)        {            return;        }        var hit = collider.gameObject;        if (hit.tag == "Player")        {            var combat = hit.GetComponent<Combat>();            combat.TakeDamage(10);            player.numBullets--;            NetworkServer.Destroy(gameObject);        }        else if (hit.tag == "Wall")        {            Debug.Log("Wall hit");            player.numBullets--;            NetworkServer.Destroy(gameObject);        }    }}
