bcad74e2240bcb0e95a27a2a625c45ed761d6e74
jake <jake@jakecoffman.com>
2016-01-27 20:29:37 -0600

    fixed bullet flashing issue by making it non-networked

--- Assets/scripts/Combat.cs
+++ Assets/scripts/Combat.cs
@@ -1,1 +1,67 @@
-using UnityEngine;using UnityEngine.Networking;using System.Collections;public class Combat : NetworkBehaviour {    public GameObject bulletPrefab;    public const float shotSpeed = 5f;    public const int maxHealth = 1;    [SyncVar]    public int health = maxHealth;    [SyncVar]    public bool isDead = false;    [HideInInspector]    [SyncVar]    public int numBullets = 0;    public int maxBullets = 5;    [Server]    public void TakeDamage(int amount)    {        health -= amount;        if (health <= 0)        {            health = 0;            isDead = true;            RpcDie();        }    }    [ClientRpc]    void RpcDie()    {        foreach (SpriteRenderer r in GetComponentsInChildren<SpriteRenderer>())        {            r.material.color = Color.black;        }    }    [Command]    public void CmdFire(GameObject player, Vector3 eulerAngles)    {        if (numBullets >= maxBullets)        {            return;        }        numBullets++;        Transform turret = player.transform.GetChild(0);        var bullet = Instantiate(bulletPrefab, turret.position + turret.up * 0.6f, Quaternion.identity) as GameObject;        // set direction of bullet and rotation        bullet.transform.rotation= turret.rotation;        //bullet.transform.eulerAngles = new Vector3(0, 0, bullet.transform.eulerAngles.z);        bullet.GetComponent<Rigidbody2D>().velocity = turret.up * shotSpeed;        bullet.GetComponent<Bullet>().player = this;        NetworkServer.Spawn(bullet);    }}
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Combat : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public const float shotSpeed = 5f;
+    public const int maxHealth = 1;
+    [SyncVar]
+    public int health = maxHealth;
+    [SyncVar]
+    public bool isDead = false;
+
+    [HideInInspector]
+    [SyncVar]
+    public int numBullets = 0;
+    public int maxBullets = 5;
+
+    [Server]
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            health = 0;
+            isDead = true;
+            RpcDie();
+        }
+    }
+
+    [ClientRpc]
+    void RpcDie()
+    {
+        foreach (SpriteRenderer r in GetComponentsInChildren<SpriteRenderer>())
+        {
+            r.material.color = Color.black;
+        }
+    }
+
+	void Fire(GameObject player, Vector3 position, Vector3 turretRotation) {
+		var bullet = Instantiate(bulletPrefab, position, Quaternion.Euler(turretRotation)) as GameObject;
+
+		// set direction of bullet and rotation
+		bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * shotSpeed;
+		bullet.GetComponent<Bullet>().player = this;
+	}
+
+    [Command]
+	public void CmdFire(GameObject player, Vector3 position, Vector3 turretRotation) // turretRoration is passed in otherwise server's rotation is used
+    {
+        if (numBullets >= maxBullets)
+        {
+            return;
+        }
+        numBullets++;
+		Fire (player, position, turretRotation);
+		RpcFire (player, position, turretRotation);
+    }
+
+	[ClientRpc]
+	public void RpcFire(GameObject player, Vector3 position, Vector3 turretRotation) {
+		if (!isServer) {
+			Fire (player, position, turretRotation);
+		}
+	}
+}
--- Assets/scripts/NetworkTank.cs
+++ Assets/scripts/NetworkTank.cs
@@ -1,1 +1,111 @@
-using UnityEngine;using UnityEngine.Networking;using System.Collections;public class NetworkTank : NetworkBehaviour {    public string playerName;    public Color color;    public float speed = 20f;    public float turnSpeed = 3.5f;	// TODO enforce server side	public float timeBetweenShots = 0.2f;    GameObject turret;    private bool isFiring = false;    private float rotation = 0f;    private Combat combat;    Rigidbody2D _rigid;    public override void OnStartLocalPlayer()    {        foreach (SpriteRenderer r in GetComponentsInChildren<SpriteRenderer>())        {            r.material.color = Color.red;        }        _rigid = GetComponent<Rigidbody2D>();    }    void Awake()    {        turret = transform.GetChild(0).gameObject;        combat = GetComponent<Combat>();    }    // called each frame    void FixedUpdate()    {        if (!isLocalPlayer || combat.isDead)        {            return;        }        Move();        Aim();        if (Input.GetMouseButton(0))        {            StartCoroutine(Fire());        }    }    void Aim()    {        Vector3 mouse_pos = Input.mousePosition;        mouse_pos.z = 0.0f;        Vector3 object_pos = Camera.main.WorldToScreenPoint(transform.position);        mouse_pos.x = mouse_pos.x - object_pos.x;        mouse_pos.y = mouse_pos.y - object_pos.y;        // -90 because my sprite is aiming up        float angle = (Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg) - 90;        Vector3 rotationVector = new Vector3(0, 0, angle);        turret.transform.rotation = Quaternion.Euler(rotationVector);    }    IEnumerator Fire()    {        if (isFiring)        {            yield break;        }        if (combat.numBullets >= combat.maxBullets)        {            yield break;        }        isFiring = true;        combat.CmdFire(gameObject, turret.transform.rotation.eulerAngles);        // TODO: Enforce server side?        yield return new WaitForSeconds(timeBetweenShots);        isFiring = false;    }    void Move()    {        float move = 0;        if(Input.GetKey(KeyCode.W))        {            move = speed;        }        if (Input.GetKey(KeyCode.S))        {            move = -speed;        }        float rotation = 0f;        if (Input.GetKey(KeyCode.A))        {            rotation += -turnSpeed;        }        if (Input.GetKey(KeyCode.D))        {            rotation += turnSpeed;        }        if (speed > 0)        {            rotation *= -1;        }        _rigid.MoveRotation(_rigid.rotation + rotation);        _rigid.AddForce(transform.up * move);    }}
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkTank : NetworkBehaviour {
+    public string playerName;
+    public Color color;
+    public float speed = 20f;
+    public float turnSpeed = 3.5f;
+
+	// TODO enforce server side
+	public float timeBetweenShots = 0.2f;
+    GameObject turret;
+
+    private bool isFiring = false;
+
+    private Combat combat;
+    Rigidbody2D _rigid;
+
+    public override void OnStartLocalPlayer()
+    {
+        foreach (SpriteRenderer r in GetComponentsInChildren<SpriteRenderer>())
+        {
+            r.material.color = Color.red;
+        }
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Awake()
+    {
+        turret = transform.GetChild(0).gameObject;
+        combat = GetComponent<Combat>();
+    }
+
+    // called each frame
+    void FixedUpdate()
+    {
+        if (!isLocalPlayer || combat.isDead)
+        {
+            return;
+        }
+        Move();
+        Aim();
+        if (Input.GetMouseButton(0))
+        {
+            StartCoroutine(Fire());
+        }
+    }
+
+    void Aim()
+    {
+        Vector3 mouse_pos = Input.mousePosition;
+        mouse_pos.z = 0.0f;
+        Vector3 object_pos = Camera.main.WorldToScreenPoint(transform.position);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        // -90 because my sprite is aiming up
+        float angle = (Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg) - 90;
+        Vector3 rotationVector = new Vector3(0, 0, angle);
+        turret.transform.rotation = Quaternion.Euler(rotationVector);
+    }
+
+    IEnumerator Fire()
+    {
+        if (isFiring)
+        {
+            yield break;
+        }
+
+        if (combat.numBullets >= combat.maxBullets)
+        {
+            yield break;
+        }
+        isFiring = true;
+
+		combat.CmdFire(gameObject, turret.transform.position + turret.transform.up * 0.6f, turret.transform.rotation.eulerAngles);
+        // TODO: Enforce server side?
+        yield return new WaitForSeconds(timeBetweenShots);
+        isFiring = false;
+    }
+
+    void Move()
+    {
+        float move = 0;
+        if(Input.GetKey(KeyCode.W))
+        {
+            move = speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move = -speed;
+        }
+
+        float rotation = 0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotation += -turnSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rotation += turnSpeed;
+        }
+        if (speed > 0)
+        {
+            rotation *= -1;
+        }
+
+        _rigid.MoveRotation(_rigid.rotation + rotation);
+        _rigid.AddForce(transform.up * move);
+    }
+}
