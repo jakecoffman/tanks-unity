987d5b0aaf1b3842761ed1707dda4f2c428a49c4
jake <jake@jakecoffman.com>
2016-04-06 20:47:57 -0500

    removed unused assets

--- Assets/scripts/Pod.cs
+++ Assets/scripts/Pod.cs
@@ -1,1 +1,24 @@
-using UnityEngine;using UnityEngine.Networking;using System.Collections;public class Pod : NetworkBehaviour {    public const int maxHealth = 2;    public int health = maxHealth;    [Server]    public void TakeDamage(int damage)    {        health -= damage;        if (health <= 0)        {            RpcDie();        }    }    [ClientRpc]    void RpcDie()    {        Destroy(gameObject);    }}
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Pod : NetworkBehaviour {
+    public const int maxHealth = 2;
+    public int health = maxHealth;
+
+    [Server]
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            RpcDie();
+        }
+    }
+
+    [ClientRpc]
+    void RpcDie()
+    {
+        Destroy(gameObject);
+    }
+}
