d1359e913326c49e614d443e969a7462a5c04362
jake <jake@jakecoffman.com>
2016-04-08 21:22:58 -0500

    player won text and delay returning to server

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
 
 public class GameManager : NetworkBehaviour {
 
     public GameObject playersText;
+    public GameObject centerText;
     Text pText;
+    Text cText;
 
     GameObject[] alivePlayers;
+    bool playerDied;
+    int secondsUntilBackToLobby = 5;
 
-    void Start()
+    void Awake()
     {
         pText = playersText.GetComponent<Text>();
+        cText = centerText.GetComponent<Text>();
     }
 
     public override void OnStartServer()
@@ -39,17 +45,23 @@ public class GameManager : NetworkBehaviour {
         aliveList.Remove(tank);
         alivePlayers = aliveList.ToArray();
         RpcAlivePlayers(alivePlayers);
-        if (alivePlayers.Length == 0)
+        if (alivePlayers.Length <= 1)
         {
-            var lobby = NetworkManager.singleton as MyNetworkLobbyManager;
-            if (lobby)
-            {
-                lobby.ServerChangeScene(lobby.lobbyScene);
-            }
-            else
-            {
-                Debug.Log("Can't get lobby manager!!!!");
-            }
+            StartCoroutine(ReturnToLobby());
+        }
+    }
+
+    IEnumerator ReturnToLobby()
+    {
+        yield return new WaitForSeconds(secondsUntilBackToLobby);
+        var lobby = NetworkManager.singleton as MyNetworkLobbyManager;
+        if (lobby)
+        {
+            lobby.ServerChangeScene(lobby.lobbyScene);
+        }
+        else
+        {
+            Debug.Log("Can't get lobby manager!!!!");
         }
     }
 
@@ -57,18 +69,31 @@ public class GameManager : NetworkBehaviour {
     void RpcAlivePlayers(GameObject[] alive)
     {
         alivePlayers = alive;
+        playerDied = true;
     }
 
     void OnGUI()
     {
         pText.text = "Players left: " + alivePlayers.Length;
+        if (playerDied)
+        {
+            playerDied = false;
+            if (alivePlayers.Length == 1)
+            {
+                cText.text = alivePlayers[0].GetComponent<Tank>().playerName + " won!";
+            }
+            else if (alivePlayers.Length == 0)
+            {
+                cText.text = "Everyone died.";
+            }
+        }
     }
 
     void OnValidate()
     {
-        if (playersText == null)
+        if (playersText == null || centerText == null)
         {
-            Debug.Log("Set players text");
+            Debug.Log("Set text GameObjects in GameManager");
         }
     }
 }
